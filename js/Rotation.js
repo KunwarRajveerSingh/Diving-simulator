623b39de140524aabc48e2fbc8295a4c5ccf6dee
KunwarRajveerSingh <rajveerpin325@gmail.com>
2022-06-01 21:27:18 +0530

    first commit

--- js/Rotation.js
+++ js/Rotation.js
@@ -1,3 +1,4 @@
+//Diver rotation component
 AFRAME.registerComponent("diver-rotation-reader", {
   schema: {
     speedOfRoation: { type: "number", default: 0 },
@@ -6,13 +7,14 @@ AFRAME.registerComponent("diver-rotation-reader", {
   init: function () {
     window.addEventListener("keydown", (e) => {
 
+      //get the data from the attributes
       this.data.speedOfRoation = this.el.getAttribute("rotation");      
       this.data.speedOfAscent = this.el.getAttribute("position");
 
       var diverRotation = this.data.speedOfRoation;      
       var diverPosition = this.data.speedOfAscent;
 
-      //controls to control the rotation of the Diver
+      //control the attributes with the Arrow Keys
       if (e.key === "ArrowRight") {
         if (diverRotation.x < 10) {
           diverRotation.x += 0.5;
